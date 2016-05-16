b6223e448dda6934d5e0067f95f32cd5a354e0d9
alice <wotupset@gmail.com>
2016-05-17 00:23:18 +0800

    Create c160517-0023.c

---
+++ day01/c160517-0023.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main(void)
+{
+	int a = 22;
+    int *aPtr=&a;//宣告為指標型態 
+
+    
+    printf("%d\n",  a);//a的內容 
+    printf("%d\n", &a);//a的參考位置 
+    aPtr=&a;//指標 指向 a的參考位置 
+    printf("%d\n",  aPtr);//傳回 a的參考位置 
+    printf("%d\n", *aPtr);//傳回 a的內容 
+    
+    return 0;
+}
