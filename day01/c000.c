3cdc44301c5790564cd597f066c93c235fbdd58d
alice <wotupset@gmail.com>
2016-05-16 23:26:10 +0800

    Create c000.c

---
+++ day01/c000.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	//印出字串 
+	printf("ㄏㄏ\n");//最簡單的輸出字串 
+	printf("%dㄏ%dㄏ\n",1,2);//printf後面可以接輸出用的變數 
+	//
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d %d\n",a,b);//
+
+	//
+	//system("pause");
+	return 0;
+} 
