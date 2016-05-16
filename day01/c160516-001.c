d893833eae1278ed519bcd2e16a8af8fa1e232bc
alice <wotupset@gmail.com>
2016-05-16 23:40:25 +0800

    Create c160516-001.c

---
+++ day01/c160516-001.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	//
+	main2();
+	//
+	return 0;
+} 
+//void
+int main2(){
+	//印出字串 
+	printf("ㄏㄏ\n");//最簡單的輸出字串 
+	printf("%dㄏ%dㄏ\n",1,2);//printf後面可以接輸出用的變數 
+	//
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d %d\n",a,b);//
+	//
+	return 0;
+}
